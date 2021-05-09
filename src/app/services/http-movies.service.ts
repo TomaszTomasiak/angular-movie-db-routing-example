bb60ba4bceb85999bd56cb7dd387bbc645e5e4ea
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-05-09 11:11:57 +0200

    implemented request get

--- src/app/services/http-movies.service.ts
+++ src/app/services/http-movies.service.ts
@@ -1,11 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Movie } from '../models/movie';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpMoviesService {
+ 
+  private url = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
+
+  getMovies(): Observable<Movie[]>{
+    return this.http.get<Movie[]>(this.url + '/movies')
+    .pipe(tap(console.log))
+  }
 }
- 
